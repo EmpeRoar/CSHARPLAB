0461d5f7e3ce199d03b3bcab77210892ca0af018
juliusbacosa <juliusbacosa@gmail.com>
2017-12-31 09:12:02 +0800

    yield #master

---
+++ YIELDLAB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YIELDLAB
+{
+    class Program
+    {
+        static Random rand = new Random();
+        static IEnumerable<int> GetRandomNumbers(int count)
+        {
+            List<int> ints = new List<int>();
+            for (int i = 0; i < count; i++)
+                ints.Add(rand.Next());
+            return ints;
+        }
+
+        static IEnumerable<int> GetRandomNumbersWithYield(int count)
+        {
+            for(int i = 0; i < count; i++)
+                yield return rand.Next();
+
+        }
+        static void Main(string[] args)
+        {
+            foreach(int num in GetRandomNumbersWithYield(10))
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
