f01d5be92705aa189a3817d35a1858faa5b776cc
Julius Bacosa <juliusbacosa@gmail.com>
2021-06-16 01:12:37 +0800

    subscrint

---
+++ csharpx/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace csharpx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = "Hello World";            
+            var s = str.Substring(0,6);
+            Console.WriteLine(s);
+        }
+    }
+}
