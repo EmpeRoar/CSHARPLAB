5b91c40662de4cd91f7e9993875106b591e7aa79
Julius Bacosa <juliusbacosa@gmail.com>
2021-07-27 12:32:22 +0800

    ...

---
+++ CSHARP/Controllers/CsharpController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CSHARP.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CsharpController : ControllerBase
+    {
+        public CsharpController()
+        {
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+    }
+}
